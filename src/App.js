8b70e61da8aa830a7d2d6063f16d11f6fec78701
namefe <smc9102@naver.com>
2025-06-05 01:19:42 +0900

    0605집

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import ClickEffect from './view/clickEffect';
 import Footer from './view/footer';
 import Header from './view/header';
 import View01 from './view/view01';
-import View01M from './view/view01M';
 import View02 from './view/view02';
 import View03 from './view/view03';
 import View04 from './view/view04';
@@ -26,7 +25,6 @@ function App() {
 */}
 
       <View01/>
-      <View01M/>
       <View02/>
       <View03/>
       <View04/>
